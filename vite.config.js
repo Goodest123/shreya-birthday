d14c1302d4983aba4e378fe67dd5b45024f7b2fd
Goodest123 <piyushkariwal123@gmail.com>
2025-09-24 06:10:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/shreya-birthday/',   // 👈 important!
+  base: '/shreya-birthday/',   
   plugins: [react()]
 })
 
